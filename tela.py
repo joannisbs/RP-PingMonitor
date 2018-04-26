cb93b163332a8706f383c27c285162439b00aba3
joannis <joannisbs@gmail.com>
2018-04-26 03:14:00 -0300

    Corrigido o Bug de fechar

--- tela.py
+++ tela.py
@@ -7,6 +7,7 @@ from Var import *
 from Thread import *
 from TestFuncions import *
 from TelaMonitoramento import *
+import tkMessageBox as messagebox
 from BancoELogs import *
 from AtualizacaoEContagem import *
 import time
@@ -17,36 +18,44 @@ Contagem()
 leBanco()
 
 
+def on_closing():
+	if messagebox.askokcancel("Quit","Quer realmente sair?"):
+		Telas.root.destroy()
+
+
 class Iniciooo:
+
 	def __init__(self,root):
 
 
-
-
-		lable1 = Label(teste, text = "Real Ponto" )
-		lable1.grid(row=0,pady=5,padx=20)
 		Relo1 = Toplevel(master=None)
 		Relo1.title("Monitor Relogios 1")
+		Relo1.protocol("WM_DELETE_WINDOW",on_closing)
 		Telas.GUI_Tela1 = TelaRelogios1(Relo1)
 
 		Monitor = Toplevel(master=None)
 		Monitor.title("Monitoramento e Controle")
-
+		Monitor.protocol("WM_DELETE_WINDOW",on_closing)
 		Telas.GUI_Monitor = TelaMonitor(Monitor)
 	
 
 		#self.destroy()
 
-teste = Tk()
 
-Iniciooo(teste)
 
-teste.wm_withdraw()
 
+
+
+Telas.root = Tk()
+
+Iniciooo(Telas.root)
+
+
+Telas.root.wm_withdraw()
 
 
 #Relo1.mainloop()
-teste.mainloop()
+Telas.root.mainloop()
 
 
 
